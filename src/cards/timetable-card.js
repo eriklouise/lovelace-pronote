00562396e8bf1ba2a883d734df26903cd57c3b43
Erik LOUISE <eriklouise@users.noreply.github.com>
2025-01-03 11:52:08 +0100

    Update timetable-card.js

--- src/cards/timetable-card.js
+++ src/cards/timetable-card.js
@@ -278,7 +278,7 @@ class PronoteTimetableCard extends LitElement {
             text-align:center;
         }
         div {
-            padding: 12px;
+            padding: 3px;
             font-weight:bold;
             font-size:1em;
         }
