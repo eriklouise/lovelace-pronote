2989ddf5ed090831e44197118a86e8d78c10bfce
Erik LOUISE <eriklouise@users.noreply.github.com>
2025-01-03 09:26:40 +0100

    Update grades-card-editor.js

--- src/editors/grades-card-editor.js
+++ src/editors/grades-card-editor.js
@@ -25,6 +25,7 @@ class PronoteGradesCardEditor extends BasePronoteCardEditor {
             ${this.buildSwitchField('Display class max', 'display_class_max', this._config.display_class_max)}
             ${this.buildSwitchField('Display new grade notice', 'display_new_grade_notice', this._config.display_new_grade_notice)}
             ${this.buildNumberField('Max grades', 'max_grades', this._config.max_grades)}
+            ${this.buildTextField('Child name', 'child_name', this._config.child_name)}
         `;
     }
 }
