5e39210393d1d697c6e6ea92c17f7f80124ef1e5
Erik LOUISE <eriklouise@users.noreply.github.com>
2025-01-03 09:26:52 +0100

    Update homework-card-editor.js

--- src/editors/homework-card-editor.js
+++ src/editors/homework-card-editor.js
@@ -18,6 +18,7 @@ class PronoteHomeworkCardEditor extends BasePronoteCardEditor {
             ${this.buildSwitchField('Current week only', 'current_week_only', this._config.current_week_only)}
             ${this.buildSwitchField('Reduce done homework', 'reduce_done_homework', this._config.reduce_done_homework)}
             ${this.buildSwitchField('Display done homework', 'display_done_homework', this._config.display_done_homework)}
+            ${this.buildTextField('Child name', 'child_name', this._config.child_name)}
         `;
     }
 }
